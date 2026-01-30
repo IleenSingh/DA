be87fa63fcf8cccb4116b36904af261738027681
ileen <ileensingh14@gmail.com>
2026-01-30 18:01:30 +0530

    commit

---
+++ 2-Modules/package/__init__.py
@@ -0,0 +1,4 @@
+#Tells Python that the folder is a package
+#custom modules or package bna skte h package folder mai
+
+#__init__.py is a file used in python to define packages and intialise their namespaces.
---
+++ 2-Modules/test.py
@@ -0,0 +1,2 @@
+from package.maths import *
+print(addition(10,22))
